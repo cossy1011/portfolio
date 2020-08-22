230c5466d2578e93a8605eccfc8100a92b88b997
yosuke <yosuke.k.323@gmail.com>
2020-08-22 15:32:31 +0900

    add particles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles-js";
 
 function App() {
+  let client_w = document.getElementById('root').clientWidth;
+  let client_h = document.getElementById('root').clientHeight;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1
+        style={{
+          color: "#FFFFFF",
+          height: "100vh",
+          width: "100vw",
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >Coming soon...</h1>
+      <Particles
+        width="100vw"
+        height="100vh"
+        params={{
+          "fps_limit": 28,
+          "particles": {
+              "collisions": {
+                  "enable": false
+              },
+              "number": {
+                  "value": (client_w + client_h) / 12,
+                  "density": {
+                      "enable": false
+                  }
+              },
+              "line_linked": {
+                  "enable": true,
+                  "distance": 200,
+                  "opacity": 0.3
+              },
+              "move": {
+                  "speed": 2
+              },
+              "opacity": {
+                  "anim": {
+                      "enable": true,
+                      "opacity_min": 0.05,
+                      "speed": 1,
+                      "sync": false
+                  },
+                  "value": 0.3
+              }
+          },
+          "retina_detect": false,
+        }}
+      />
     </div>
   );
 }
