0c1edda9b1e0ba72150707712a6edfc9e094a675
yosuke <yosuke.k.323@gmail.com>
2020-10-03 11:12:03 +0900

    remove react-particle-js

--- src/MobileView.js
+++ src/MobileView.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import Particles from "react-particles-js";
+import React  from 'react';
 import "./MobileView.css"
 import styled, { keyframes } from 'styled-components';
 
 function MobileView() {
-    let client_w = document.getElementById('root').clientWidth;
-    let client_h = document.getElementById('root').clientHeight;
     
     const fadeIn = keyframes`
         0% {
@@ -50,42 +47,6 @@ function MobileView() {
         }}>
             {TitleList}
         </h1>
-      <Particles
-        width="100vw"
-        height="100vh"
-        params={{
-          "fps_limit": 28,
-          "particles": {
-              "collisions": {
-                  "enable": false
-              },
-              "number": {
-                  "value": (client_w + client_h) / 12,
-                  "density": {
-                      "enable": false
-                  }
-              },
-              "line_linked": {
-                  "enable": true,
-                  "distance": 200,
-                  "opacity": 0.3
-              },
-              "move": {
-                  "speed": 2
-              },
-              "opacity": {
-                  "anim": {
-                      "enable": true,
-                      "opacity_min": 0.05,
-                      "speed": 1,
-                      "sync": false
-                  },
-                  "value": 0.3
-              }
-          },
-          "retina_detect": false,
-        }}
-      />
     </div>
   );
 }
